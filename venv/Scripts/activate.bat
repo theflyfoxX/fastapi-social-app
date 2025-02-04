31d28d7932c9ecbcacef30d114eedbb6124a4f2a
SaadOuardi <saad.ouardi.developer@gmail.com>
2025-02-04 02:43:36 +0100

    v2.4.25.2.43

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Admin\Desktop\PERSO\Perso 2\programming\coding\github\fastapi-course\backend\venv
+set VIRTUAL_ENV=C:\Users\Admin\Desktop\PERSO\Perso 2\programming\coding\github\fastapi-course\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
